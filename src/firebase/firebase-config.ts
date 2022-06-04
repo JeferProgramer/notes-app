8ec57f8dd0c9fe15788abba3a3bc7abb3733fe29
JeferProgramer <jefer.talento.11@gmail>
2022-06-04 09:41:00 -0500

    Start Types

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtp71BD2xt0M7Kc7ftugaYOY2Fgreq3A8",
+    authDomain: "notes-app-ab1ee.firebaseapp.com",
+    databaseURL: "https://notes-app-ab1ee-default-rtdb.firebaseio.com",
+    projectId: "notes-app-ab1ee",
+    storageBucket: "notes-app-ab1ee.appspot.com",
+    messagingSenderId: "158272833521",
+    appId: "1:158272833521:web:e8acf6c105d698bcaba650"
+};
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore();
---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+export const types = {
+    login: "[AUTH] Login",
+    register: "[AUTH] Register",
+    logout: "[AUTH] Logout",
+
+    uiSetError: "[UI] Set Error",
+    uiRemoveError: "[UI] Remove Error",
+    uiStartLoading:"[UI] Start Loading",
+    uiFinishLoading:"[UI] Finish Loading",
+
+    notesAddNew: "[Notes] New Note",
+    notesActive:"[Notes] Set active Note",
+    notesLoad: "[Notes] Load notes",
+    notesUpdated: "[Notes] Update Notes",
+    notesFileUrl:"[Notes] Update image URL",
+    notesDelete:"[Notes] Delete Note",
+    notesLoagoutCleaning:"[Notas] Logout Cleaning",
+}

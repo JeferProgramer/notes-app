675ed1952777fbe6e66df6ecee2b90d27b9f0335
JeferProgramer <jefer.talento.11@gmail>
2022-06-04 10:31:50 -0500

    start login with email and password function

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -3,7 +3,23 @@ import {firebase, gogleAuthProvider} from "../firebase/firebase-config"
 import { finishLoading, startLoading } from "./ui";
 import Swal from "sweetalert2";
 
-const login = ( uid: string,displayName: string) => ({
+const login = ( uid: string|undefined,displayName: string | undefined | null) => ({
     type: types.login,
     payload:{uid,displayName},
 });
+const startLoadingWithEmailPassword = (email:string, password:string) => {
+    return (dispatch:any) => {
+        dispatch(startLoading());
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email,password)
+            .then(({user}) => {
+              dispatch(login(user?.uid, user?.displayName))
+              dispatch(finishLoading());
+            })
+            .catch((e) => {
+                Swal.fire("Error", e.message, "error")
+                dispatch(finishLoading());
+            })
+    }
+}
